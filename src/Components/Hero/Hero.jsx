09ab402b16a3ccba676b347f39e4f086b38adacd
Brrryyyan <johnbryanb3@gmail.com>
2025-03-25 20:27:34 +0800

    enh: modified the heading

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,11 +6,11 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
         <img src={profile_img} alt="" />
-        <h1><span>Hello, I'm Bryan,</span> frontend developer from Cebu City</h1>
+        <h1><span>Hello, I'm Bryan,</span> BSIT graduate from Cebu City</h1>
         <p>I'm passionate about building user-friendly interfaces and creating engaging experiences.</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect with me</div>
-            <div className="hero-resume">My resume</div>
+            <div className="hero-connect">My resume</div>
+            
         </div>
     </div>
   )
